526b20380062651822d606685072150a338dfb89
Mohamed Zeina <toxiczeina@gmail.com>
2024-12-29 18:39:39 +0300

    fetching a single post - fetching multiple posts from database finished

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -3,26 +3,24 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/duck.png',
-        creator: {
-          name: 'Mohamed',
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res
+        .status(200)
+        .json({ message: 'Fetched posts successfully!', posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, entered data is incorrect');
+    const error = new Error('Validation failed, entered data is incorrect.');
     error.statusCode = 422;
     throw error;
   }
@@ -48,5 +46,27 @@ exports.createPost = (req, res, next) => {
         err.statusCode = 500;
       }
       next(err); // Since we're in an async, we can't throw an error like above
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      console.log(post);
+      res
+        .status(200)
+        .json({ message: 'Post fetched successfully!', post: post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
     });
 };
