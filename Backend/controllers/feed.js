57c540668f83ec1255e6d65671b8ff68c99aedd8
Mohamed Zeina <toxiczeina@gmail.com>
2025-01-03 19:27:58 +0300

    added pagination to the posts feed

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -6,11 +6,25 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const postsPerPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * postsPerPage)
+        .limit(postsPerPage);
+    })
     .then((posts) => {
       res
         .status(200)
-        .json({ message: 'Fetched posts successfully!', posts: posts });
+        .json({
+          message: 'Fetched posts successfully!',
+          posts: posts,
+          totalItems: totalItems,
+        });
     })
     .catch((err) => {
       if (!err.statusCode) {
