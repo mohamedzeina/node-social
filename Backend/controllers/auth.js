3979af78fd9319fd47f187bc3385eb3af69628b0
Mohamed Zeina <toxiczeina@gmail.com>
2025-02-09 01:26:46 +0300

    used the jwt token to validate fetching posts for a user

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -7,7 +7,7 @@ const User = require('../models/user');
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error('Validation failed.');
+    const error = new Error('Validation Failed!');
     error.statusCode = 422;
     error.data = errors.array();
     throw error;
@@ -26,7 +26,7 @@ exports.signup = (req, res, next) => {
       return user.save();
     })
     .then((result) => {
-      res.status(201).json({ message: 'User created!', userId: result._id });
+      res.status(201).json({ message: 'User Created!', userId: result._id });
     })
     .catch((err) => {
       if (!err.statusCode) {
@@ -52,7 +52,7 @@ exports.login = (req, res, next) => {
     })
     .then((isEqual) => {
       if (!isEqual) {
-        const error = new Error('Wrong password!');
+        const error = new Error('Wrong Password!');
         error.statusCode = 401;
         throw error;
       }
