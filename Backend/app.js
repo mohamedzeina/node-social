97f3ac8f19b2a78c97542efa9b89f78116e0dbf4
Mohamed Zeina <toxiczeina@gmail.com>
2025-02-10 14:51:58 +0300

    shared the io instance across files

--- Backend/app.js
+++ Backend/app.js
@@ -61,11 +61,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then((result) => {
     const server = app.listen(8080);
-    const io = require('socket.io')(server, {
-      cors: {
-        origin: 'http://localhost:3000',
-      },
-    });
+    const io = require('./socket').init(server);
     io.on('connection', (socket) => {
       console.log('Client Connected!');
     });
