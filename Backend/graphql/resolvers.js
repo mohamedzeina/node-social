958549cc63ae7edac5d7e8bba16d3ea99fdf66ee
Mohamed Zeina <toxiczeina@gmail.com>
2025-02-11 20:41:25 +0300

    added input validation when creating a new user

--- Backend/graphql/resolvers.js
+++ Backend/graphql/resolvers.js
@@ -1,10 +1,28 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const User = require('../models/user');
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'E-Mail is Invalid!' });
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: 'Password too Short!' });
+    }
+
+    if (errors.length > 0) {
+      const error = new Error(errors[0].message);
+      throw error;
+    }
+
     const existingUser = await User.findOne({ email: userInput.email });
+
     if (existingUser) {
       const error = new Error('User Exists Already!');
       throw error;
