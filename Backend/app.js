1bb9fc2e6a04721fd32755e5f596ba69753032a8
Mohamed Zeina <toxiczeina@gmail.com>
2025-04-01 14:04:25 +0300

    connected frontend with graphql to perform signus

--- Backend/app.js
+++ Backend/app.js
@@ -43,6 +43,10 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 }); // Every response sent by the server will have these headers
 
