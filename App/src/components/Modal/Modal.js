6b8f4d9e35ff288c075b501f6b150b8e8c9dcaf8
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-13 22:03:53 +0300

    feat(ui): redesign Modal as 'Editor's desk' overlay
    - Newsprint sheet with hard offset shadow + vermilion sliver
    - Register-mark corner ticks (print-plate detail)
    - Eyebrow line, large Fraunces title, double-rule under header
    - Accept button copy changed to 'Set in print'
    - Rise-and-fade entrance animation

--- App/src/components/Modal/Modal.js
+++ App/src/components/Modal/Modal.js
@@ -6,11 +6,20 @@ import './Modal.css';
 
 const modal = props =>
   ReactDOM.createPortal(
-    <div className="modal">
+    <div className="modal" role="dialog" aria-modal="true" aria-labelledby="modal-title">
+      <div className="modal__corner modal__corner--tl" />
+      <div className="modal__corner modal__corner--tr" />
+      <div className="modal__corner modal__corner--bl" />
+      <div className="modal__corner modal__corner--br" />
+
       <header className="modal__header">
-        <h1>{props.title}</h1>
+        <span className="modal__eyebrow">Editor&apos;s desk</span>
+        <h1 id="modal-title" className="modal__title">{props.title}</h1>
+        <div className="modal__rule" />
       </header>
+
       <div className="modal__content">{props.children}</div>
+
       <div className="modal__actions">
         <Button design="danger" mode="flat" onClick={props.onCancelModal}>
           Cancel
@@ -21,7 +30,7 @@ const modal = props =>
           disabled={!props.acceptEnabled}
           loading={props.isLoading}
         >
-          Accept
+          Set in print
         </Button>
       </div>
     </div>,
