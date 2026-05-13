c4e809e868c3470ccf5f544a9e8988267ed9002d
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-13 22:01:37 +0300

    feat(ui): redesign Loader as typographic 'Setting type...' state
    - Replace four spinning rings with a monospace caption sandwiched
      between two animated hairline rules
    - Each letter pulses sequentially in vermilion (typesetting metaphor)
    - Reads as a status message instead of an abstract spinner

--- App/src/components/Loader/Loader.js
+++ App/src/components/Loader/Loader.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 import './Loader.css';
 
-const loader = props => (
-  <div className="loader">
-    <div />
-    <div />
-    <div />
-    <div />
+const loader = () => (
+  <div className="loader" role="status" aria-live="polite">
+    <div className="loader__rule" />
+    <div className="loader__label">
+      <span>S</span>
+      <span>e</span>
+      <span>t</span>
+      <span>t</span>
+      <span>i</span>
+      <span>n</span>
+      <span>g</span>
+      <span>&nbsp;</span>
+      <span>t</span>
+      <span>y</span>
+      <span>p</span>
+      <span>e</span>
+      <span>&hellip;</span>
+    </div>
+    <div className="loader__rule" />
   </div>
 );
 
